080cd05d15df61ff82120a6aaf86849fc7fdcbb3
89oinotna <a.z.zegarelli@live.it>
2020-12-29 15:11:38 +0100

    +python needs some adjustment on the concurrency (writing)

---
+++ second_assignment/python/main.py
@@ -0,0 +1,94 @@
+import os
+
+from tabulate import tabulate
+import time
+import threading
+
+
+def benchmark(fun, warmups=0, iter=1, verbose=False, csv_file=None):
+    def print_table(avg, var, iterations):
+        res = "{}".format(tabulate([[avg, var]], headers=['AVG', 'Variance']))
+        if verbose:
+            res = "{}\n{}\n".format(res, (tabulate([[i, it[1], it[0]] for i, it in enumerate(iterations)],
+                                                   headers=['run num', 'is warmup', 'timing'])))
+        print(res)
+
+    def csv(iterations):
+        with open(csv_file, "a+", newline='') as f:
+            f.write("run num, is warmup, timing\n")
+            for i, it in enumerate(iterations):
+                f.write("{}\n".format(", ".join([str(i), it[1], str(it[0])])))
+
+    def running_time(*args, **kwargs):
+        start = time.perf_counter()
+        fun(*args, **kwargs)
+        end = time.perf_counter()
+        return end - start
+
+    def wrapper(*args, **kwargs):
+        wup = []
+        exe = []
+        avg = 0
+        for i in range(warmups):
+            wup.append((running_time(*args, **kwargs), "yes"))
+        for i in range(iter):
+            rt = running_time(*args, **kwargs)
+            avg += rt
+            exe.append((rt, "no"))
+        avg /= iter
+        var = 0
+        for (ns, tp) in exe:
+            var += (ns - avg) * (ns - avg)
+        var /= iter
+        wup.extend(exe)
+        print_table(avg, var, wup)
+        if csv_file is not None:
+            csv(wup)
+
+    return wrapper
+
+
+class MyThread(threading.Thread):
+    def __init__(self, fun, *args, **kwargs):
+        super().__init__()
+        self.fun = fun
+        self.args = args
+        self.kwargs = kwargs
+
+    def run(self):
+        self.fun(*self.args, **self.kwargs)
+
+
+def test(fun, *args, **kwargs):
+    def multi(fun, n_thread, *args, **kwargs):
+        t_list = []
+        for i in range(n_thread):
+            t_list.append(MyThread(fun, *args, **kwargs))
+
+        def run():
+            print("\n\nStarting test with: {} thread\n".format(n_thread))
+            for i in t_list:
+                i.start()
+            for i in t_list:
+                i.join()
+
+        return run
+
+    single = benchmark(fun, iter=16, verbose=True, csv_file="f_1_16.csv")
+    single(*args, **kwargs)
+    eight_2 = multi(benchmark(fun, iter=8, verbose=True, csv_file="f_2_8.csv"), 2, *args, **kwargs)
+    four_4 = multi(benchmark(fun, iter=4, verbose=True, csv_file="f_4_4.csv"), 4, *args, **kwargs)
+    two_8 = multi(benchmark(fun, iter=2, verbose=True, csv_file="f_8_2.csv"), 8, *args, **kwargs)
+    eight_2()
+    four_4()
+    two_8()
+
+
+def fib(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    return fib(n - 1) + fib(n - 2)
+
+test(fib, 2)
